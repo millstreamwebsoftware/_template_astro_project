f95ffeb476665b70758538fff424197d9aa7bbcb
oliverlynch <59594611+oliverlynch@users.noreply.github.com>
2025-01-28 16:56:10 +0800

    Update template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import bookshop from "@bookshop/astro-bookshop";
-import favicon from "astro-favicons";
+import favicons from "astro-favicons";
 import preferences from "/data/preferences.json";
 
 // https://astro.build/config
@@ -19,26 +19,13 @@ export default defineConfig({
         },
       },
     },
-    favicon({
-      masterPicture: preferences.general.favicon,
-      emitAssets: false, // Output image files (can be disabled after files built and committed to git)
-
-      appName: preferences.general.site_name,
-      appShortName: null,
-      appDescription: preferences.general.default_site_description,
-      lang: "en-AU",
-      path: "ico",
-
-      background: "#fff",
-      theme_color: "#4daaa8",
-
-      faviconsDarkMode: false, // default `true`, Make favicon compatible with light and dark modes
-      cacheBustingQueryParam: "", // update this to "v=1" or similar to encourage browsers to refresh the favicon
-      appleStatusBarStyle: "default",
-    }),
+    favicons(),
   ],
   redirects: {
     "/manager": "/404",
   },
   scopedStyleStrategy: "attribute",
+  image: {
+    domains: ["dam.cms.io"],
+  },
 });
